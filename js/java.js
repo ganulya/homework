8d73e056c3d78ae2b5e0afd32d23752437b95d78
ganulya <Ann.skywalker12@gmail.com>
2021-03-28 18:14:47 +0300

    :(((((

---
+++ js/java.js
@@ -0,0 +1,9 @@
+const result = document.getElementById('result').value;
+const num = document.getElementById('num').value;
+const eq = document.getElementById('equal').value;
+const op = document.getElementById('operator').value;
+
+function display() {
+    result.innerHTML = num.value;
+}
+
