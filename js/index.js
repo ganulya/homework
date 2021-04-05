ddd45ae9b7123fc334bd19b9533632b5a1e80e99
ganulya <Ann.skywalker12@gmail.com>
2021-04-05 23:43:20 +0300

    дополнительно

---
+++ js/index.js
@@ -0,0 +1,51 @@
+////недополнительно
+////#1
+//const arr1 = [1, 2, 3, 4, 5];
+//arr1.splice(1, 2);
+//console.log(arr1);
+//
+////#2
+//const arr2 = [1, 2, 3, 4, 5];
+//arr2.reverse();
+//console.log(arr2);
+//
+////#3
+//const arr3 = [11, 22, 33, 44, 55];
+//const newArr3 = [];
+//
+//for (let i = arr3.length - 1; i >= 0; i--) {
+//    newArr3.push(arr3[i]);
+//}
+//
+//console.log(arr3);
+//console.log(newArr3);
+//
+//#4 не работает, не понимаю :С
+//const arr4 = [6, 7, 8, 9, 10];
+//const arr5 = [6, 7, 8, 9, 10];
+
+//const arr4 = [6, 4, 8, 2, 1];
+//const arr5 = [6, 4, 8, 2, 1];
+//
+//function compareArrs() {
+//    
+//    if (arr4 !== arr5) {
+//            return false;    
+//        } 
+//    
+//    return true;
+//}
+//
+//console.log(compareArrs());
+
+//#5 ......хз....
+//let arr6 = [];
+//
+//for (let i = 0; i <= 99; i++) {
+////    arr6[i] = i + 1;
+//    if (arr6[i] % 3 == 0) {
+//    }
+//    
+//}
+//console.log(arr6);
+//
