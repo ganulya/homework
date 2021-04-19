3cf4b41c4480bc9bbee53c902722490f2fc93e4a
ganulya <Ann.skywalker12@gmail.com>
2021-04-19 21:06:03 +0300

    все что могла:(

--- js/index.js
+++ js/index.js
@@ -1,125 +1,22 @@
-//массив не с объектами, а с массивами(массив массивов)
-const users = [
-    {
-        name: 'leha',
-        age: 16,   
-    },
-    {
-        name: 'rob',
-        age: 19,   
-    },
-    {
-        name: 'kate',
-        age: 20,   
-    }
-    
-]
+//// #1 сортировка чисел в массиве по убыванию
+//const arr = [4, 2, 6, 1, 3, 5, 7];
+//
+//const compareNumbers = arr.sort((prev, current) => {
+//    return current - prev
+//}); 
+//
+//console.log(compareNumbers);
 
-const newArr = [];
-newArr.push(Object.keys(users));
-newArr.push(Object.values(users));
-console.log(newArr);
+//// #2 сортирует массив от ложных значений
+//const arr = ['bla', NaN, 2, 6, null, 0, 1, undefined, 3, 'nope', '', 7];
+//
+//const filterFalse = arr.filter(item => item ? item : null); 
+//
+//console.log(filterFalse);
 
-//#1 no
-let input = document.getElementById('inpV');
-let changeNum = document.getElementById('change');
-
-function colonOdd(num) {
-    
-}
-
-//#2 & #3
-let arr = ['dwtw', 23, 5, 11, 13, 33, 'sbtwotl', 93];
-
-function getFirst(array, n) {
-    let arrFirst = array.splice(0, n);
-    console.log(arrFirst);
-}
-
-getFirst(arr, 1);
-
-function getLast(array, n) {
-    let arrLast = array.splice(-n);
-    console.log(arrLast);
-}
-
-getLast(arr, 1);
-
-
-//#5 
-let aarr = [4, 4];
-let sum = 0;
-
-function sumOfSquares(arr) {
-    for (let j = 0; j < aarr.length; j++) {
-        sum += Math.pow(aarr[j], 2);
-    }
-    console.log(sum);
-}
-sumOfSquares(arr);
-
-//#6  
-const ppl0 = [
-    {
-        name: 'Leha',
-        age: 27
-    },
-    {
-        name: 'Max',
-        age: 25
-    },
-    {
-        name: 'Elvis',
-        age: 31
-    },
-    {
-        name: 'Roll',
-        age: 30
-    },
-]
-console.table(ppl0);
-
-let userAge = [];
-
-for (let r = 0; r < ppl0.length; r++) {
-    
-    if (ppl0[r].age < 30) {
-        userAge.push(ppl0[r]);
-    }
-}
-
-console.log(userAge);
-
-//#7 
-
-const ppl = [
-    {
-        name: 'Leha',
-        age: 27
-    },
-    {
-        name: 'Max',
-        age: 25
-    },
-    {
-        name: 'Elvis',
-        age: 31
-    },
-    {
-        name: 'Roll',
-        age: 30
-    },
-]
-
-console.table(ppl);
-
-function propertyValue(array, key) {
-    let valueArr = [];
-    
-    for (let k = 0; k < array.length; k++) {
-        valueArr.push(array[k][key])
-    }
-    console.log(valueArr);
-}
-
-propertyValue(ppl, 'age');
+//// #6 массив строк > массив с длинами строк
+//const arr = ['bla', 'hellno', 'night', 'nope'];
+//
+//let howlong = arr.map(item => item.length);
+//
+//console.log(howlong);
