96c628c04d8ed0d95e7c4d52d8ec2da7f8733368
ganulya <Ann.skywalker12@gmail.com>
2021-04-25 17:07:06 +0300

    the rest of it

--- js/index.js
+++ js/index.js
@@ -14,9 +14,46 @@
 //
 //console.log(filterFalse);
 
-//// #6 массив строк > массив с длинами строк
+////3 функция propertyValue(array, key) кот принимает аргументы массив и имя свойстваб а замтем возвращает все значения этого свойства из массива объектов
+//const users = [
+//    {
+//        name: 'lex',
+//        age: 31
+//    },
+//    {
+//        name: 'max',
+//        age: 25
+//    },
+//];
+//
+//const propertyValue = (array, key) => array.map( item => item[key]);
+//const newArr = propertyValue(users, 'age');
+//console.log(newArr);
+
+//// #4 массив строк > массив с длинами строк хз не работает 
+//const myStr = ['bla', 'hellno', 'night', 'nope'];
+//const getLenghs = str => str.split(' ').reduce( (acc, current) => ([...acc, current.length]), []);
+//const lengthArr = getLenghs(myStr);
+//console.log(lengthArr);
+
+//// #6 массив строк > массив с длинами строк MAP
 //const arr = ['bla', 'hellno', 'night', 'nope'];
 //
 //let howlong = arr.map(item => item.length);
 //
 //console.log(howlong);
+
+//// 7 введенные в инпут числа заносятся в массив по нажатию на кнопку, по нажатию на другую кнопку выводится отсортированный массив на экран
+
+const result = document.getElementById('result');
+const inp = document.getElementById('inp');
+let arr = [];
+
+const add = () => {
+    arr.push(+inp.value);
+    inp.value = '';
+}
+
+const show = () => {
+    result.innerHTML = arr.sort( (prev, current) => prev > current ? 1 : -1).toString();
+}
