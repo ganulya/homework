f4ac6cad80a0482d031dc6c35f01ff565e07ff98
ganulya <Ann.skywalker12@gmail.com>
2021-05-03 15:57:17 +0300

    hz

--- js/index.js
+++ js/index.js
@@ -1,21 +1,79 @@
-//#1 создать класс и метод getSalary
-class Worker {
-    constructor(name, surname, rate, days) {
-        this.name = name;
-        this.surname = surname;
-        this.rate = rate;
-        this.days = days;
-    }
-    getSalary() {
-        return this.rate * this.days;
-    }
-}
+////#1 создать класс и метод getSalary
+//class Worker {
+//    constructor(name, surname, rate, days) {
+//        this.name = name;
+//        this.surname = surname;
+//        this.rate = rate;
+//        this.days = days;
+//    }
+//    getSalary() {
+//        return this.rate * this.days;
+//    }
+//}
+//
+//const worker1 = new Worker('michail', 'bulgakov', 23, 31);
+//console.log(worker1);
+//console.log(worker1.getSalary());
 
-const worker1 = new Worker('michail', 'bulgakov', 23, 31);
-console.log(worker1);
-console.log(worker1.getSalary());
+////#2 сделать все свойства приватными, а для их чтения сделать методы-геттеры
+//class Work {
+//    
+//    #rate = 40;
+//    
+//    get rate() {
+//        return this.#rate;
+//    }
+//    
+//    constructor (days) {
+//        this.days = days;
+//    }
+//    
+//    getSalary() {
+//        return this.#rate * this.days;
+//    }
+//}
+//
+//class Worker extends Work {
+//    constructor (name, surname, days) {
+//        super(days);
+//        this.name = name;
+//        this surname = surname;
+//    }
+//}
+//
+//const worker1 = new Worker ('Mikhail', 'H', 22);
+//worker1.getSalary();
+//console.log(worker1.getSalary());
 
-//#2 сделать все свойства приватными, а для их чтения сделать методы-геттеры
 
-//#3 для свойств rate & days сделать методы-сеттеры
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
